1cfc6de0857dc7f8917e7088fcaada298d4aa7e7
Brian Takita <brian.takita@gmail.com>
2019-12-02 23:50:18 -0500

    fix: autonomous tsconfig.json

--- lib.ts
+++ lib.ts
@@ -1,6 +1,6 @@
 import { pick } from '@ctx-core/object'
 import { log } from '@ctx-core/logger'
-const logPrefix = '@ctx-core/security/lib.js'
+const logPrefix = '@ctx-core/security/lib'
 export function pick__whitelist(ctx, ...keys) {
 	log(`${logPrefix}|pick__whitelist`)
 	return pick(ctx, ...keys)
